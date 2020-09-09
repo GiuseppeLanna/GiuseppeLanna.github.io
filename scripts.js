4d1354e31ec26de9229367a72b36bea7b852e145
Giuseppe Alma <giuseppelanna2000@users.noreply.github.com>
2020-09-09 20:55:47 -0300

    asdf
    Agora mostra s'ao dois cronômetros

--- scripts.js
+++ scripts.js
@@ -5,13 +5,21 @@ var heavy;
 function tick()
 {
     time++;
-    mega.innerHTML = time % 35;
-    heavy.innerHTML = time % 25;
+    var heavyCounter = time + 25;
+    var megaCounter = time + 35;
+    heavy.innerHTML = parseInt(heavyCounter / 60) + ":" + heavyCounter % 60;
+    mega.innerHTML = parseInt(megaCounter / 60) + ":" + megaCounter % 60;
 }
 function load()
 {
+    var delay = parseInt(prompt("Escolha o tempo a ser esperado antes que o cronômetro inicie", "5"))
+    time = -delay;
+
+    var heavyCounter = time + 25;
+    var megaCounter = time + 35;
     mega = document.getElementById("mega");
     heavy = document.getElementById("heavy");
-    console.log("carregou");
+    heavy.innerHTML = parseInt(heavyCounter / 60) + ":" + heavyCounter % 60;
+    mega.innerHTML = parseInt(megaCounter / 60) + ":" + megaCounter % 60;
     setInterval(() => tick(), 1000);
 }
