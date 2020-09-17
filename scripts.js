64d01035d0c1485d22f40d70b4079116a4b937ad
Giuseppe Alma <giuseppelanna2000@users.noreply.github.com>
2020-09-16 22:40:23 -0300

    mandando tudo
    asdfasdf

--- scripts.js
+++ scripts.js
@@ -1,25 +1,58 @@
 var time = 0;
-var mega;
-var heavy;
+var mega = 0;
+var heavy = 0;
 
-function tick()
+function main()
 {
-    time++;
-    var heavyCounter = time + 25;
-    var megaCounter = time + 35;
-    heavy.innerHTML = parseInt(heavyCounter / 60) + ":" + heavyCounter % 60;
-    mega.innerHTML = parseInt(megaCounter / 60) + ":" + megaCounter % 60;
+    var delay = parseInt(prompt("Escolha o tempo a ser esperado antes que o cronômetro inicie.", "5"));
+    if (isNaN(delay))
+        delay = 0;
+
+    setTimeout(start, 1000 * delay);
+    showCountdown(delay);
 }
-function body_load()
-{
-    var delay = parseInt(prompt("Escolha o tempo a ser esperado antes que o cronômetro inicie.", "5"))
-    time = -delay;
 
-    var heavyCounter = time + 25;
-    var megaCounter = time + 35;
-    mega = document.getElementById("mega");
-    heavy = document.getElementById("heavy");
-    heavy.innerHTML = parseInt(heavyCounter / 60) + ":" + heavyCounter % 60;
-    mega.innerHTML = parseInt(megaCounter / 60) + ":" + megaCounter % 60;
-    setInterval(() => tick(), 1000);
+function showCountdown(t)
+{
+    if (t<=0)
+        return;
+
+    const show = function()
+    {
+        if(t==0)
+            return;
+        //N"ao vai ser exatamente isso que vou querer fazer.
+        document.getElementById('armor').innerHTML = t;
+        document.getElementById('megahealth').innerHTML = t;
+    }
+
+    show();
+    
+    var counter = setInterval(() => {
+        if (--t == 0)
+            clearInterval(counter);
+        show();
+    }, 1000);
+}
+
+function start()
+{
+    var time = 0;
+    var armor = document.getElementById('armor');
+    var megahealth = document.getElementById('megahealth');
+
+    const update = function()
+    {
+        var armorcounter = time + 25;
+        var megacounter = time + 35;
+        armor.innerHTML = parseInt(armorcounter / 60) + ":" + armorcounter % 60;
+        megahealth.innerHTML = parseInt(megacounter / 60) + ":" + megacounter % 60;
+    }
+
+    update();
+
+    var counter = setInterval(() => {
+        time++;
+        update();
+    }, 1000);
 }
